9b5921befea788a9e90b61e240bf48a2c0d365d1
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-11-02 21:53:33 +0100

    add project

---
+++ PiList/PiList/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  PiList
+//
+//  Created by Kostiantyn Koval on 02/11/15.
+//  Copyright © 2015 Kostiantyn Koval. All rights reserved.
+//
+
+import Foundation
+
+print("Hello PiList")
+
+let arguments = Process.arguments
+print(arguments)
+
+
