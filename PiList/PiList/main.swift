3ef3a5e50d0dffac925f4ff2af17c2c7be3bf14e
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-11-03 19:07:17 +0100

    use full example .plist

--- PiList/PiList/main.swift
+++ PiList/PiList/main.swift
@@ -10,43 +10,14 @@ import Foundation
 
 print("Hello PiList")
 
-func handleArray(content: [AnyObject]) {
-  print(content)
-}
+func handleElement(element: AEXMLElement) {
+  print("name: \(element.name), value: \(element.value), children: \(element.children)")
 
-func handleDictionary(content: [String : AnyObject]) {
-  print(content)
-
-//  let keys = content.keys
-//  print(keys)
-
-  let values = content.values
-//  print(values)
-
-  for val in values {
-
-    print("val: \(val) type: \(val.dynamicType)")
-    switch val {
-
-    case is [AnyObject]: print("Array")
-    case is [String : AnyObject]: print("Dictionary")
-
-    case is String: print("String")
-    case is NSDate: print("Date")
-    case is NSData: print("Data")
-
-    case is Double: print("Double")
-    case is Int: print("Int")
-
-    case is Bool: print("Bool")
-
-      default: print("default")
-//    case val as Bool: print("Bool")
-//    case val as Bool: print("Bool")
-    }
+  for child in element.children {
+    handleElement(child)
   }
+  print("")
 }
-
 
 //MARK:- Main
 
@@ -69,24 +40,20 @@ let arg1 = "Resource/Dictionary.plist"
 let rootPath = NSURL(fileURLWithPath: arg0)
 let filePath = rootPath.URLByAppendingPathComponent(arg1)
 
-print(rootPath)
-print(filePath)
+//print(rootPath)
+//print(filePath)
 
-guard let data = NSData(contentsOfURL: filePath) else { exit(0) }
-let doc = try? AEXMLDocument(xmlData: data)
-print(doc?.xmlString)
-
-
-if let plist = NSDictionary(contentsOfURL: filePath) {
-  print(plist)
-  handleDictionary(plist as! [String : AnyObject])
-} else if let plist = NSArray(contentsOfURL: filePath) {
-  print("handle Array")
-  handleArray(plist as [AnyObject])
-  //
-} else {
-  print("Can't read \(arguments[1]) .plist file")
+guard let data = NSData(contentsOfURL: filePath) else {
+  print("Can't read .plist file at: \(filePath)")
   exit(0)
 }
 
+do {
+  let xmlDoc = try AEXMLDocument(xmlData: data)
+//  print(xmlDoc.xmlString)
+//  print("")
+  handleElement(xmlDoc.root.children.first!)
 
+} catch {
+  print("Error: \(error)")
+}
