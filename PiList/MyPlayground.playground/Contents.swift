98078c2d3d7ba3e11156cda730d4d54983cf73da
Kostiantyn Koval <konstantin.koval1@gmail.com>
2015-11-07 13:32:27 +0100

    save playgrounds :)

--- PiList/MyPlayground.playground/Contents.swift
+++ PiList/MyPlayground.playground/Contents.swift
@@ -2,38 +2,82 @@
 struct PList {
   let version: Double
   let content: String
+
+  let rootElement: ContainerType
+
+  init(version: Double, content: String) {
+    self.version = version
+    self.content = content
+
+    //FIXME:
+    rootElement = .Array
+  }
 }
 
+enum ContainerType: String {
+  case Dictionary = "dict"
+  case Array = "array"
+}
+
+enum ValueType: String {
+  // Bool
+  case BoolTrue = "true"
+  case BoolFalse = "false"
+
+  case Data = "data"
+  case Date = "date"
+  case String = "string"
+
+  //Number
+  case Int = "integer"
+  case Double = "real"
+}
+
+struct Tokenizer {
+  private static let closeToken = ">"
+
+  static func get(content: String, token: String) -> String? {
+    let openToken = "<\(token)"
+
+    guard let
+      open = content.rangeOf(openToken),
+      close = content.rangeOf(closeToken) else { return nil }
+
+    open
+    close
+//    let tokenRange = open.startIndex..<close.startIndex
+    let a = content[open]
+    return a
+  }
+}
 
 struct PListParser {
 
   static func parse(content: String) -> PList {
-    
+
+    // Get <plist> tag and content
+    plistData(content)
+
     let plist = PList(version: 1.0, content: content)
     return plist
   }
-}
 
-
-
-
-
-struct Tokenizer {
-  let content: String
-
-  func get(token: String) -> String? {
-
-    let open = "<\(token)"
-    let close = ">"
-
-    let range = content.rangeOf(open)
-    let closeRange = content.rangeOf(close)
-    let a = content[range!]
-
-
-    return a
+  static func plistData(content: String) -> (version: Double, content: String) {
+    let plistToken = Tokenizer.get(content, token: "plist")
+    return (0, "")
   }
 }
+
+let array = Bundle.plist("Array")!
+PListParser.parse(array)
+
+
+
+
+
+
+
+
 
 
 
--- PiList/MyPlayground.playground/Sources/Bundle.swift
+++ PiList/MyPlayground.playground/Sources/Bundle.swift
@@ -1,2 +1,10 @@
 import Foundation
 
+public class Bundle {
+
+  public static func plist(x: String) -> String? {
+    let path = NSBundle.mainBundle().bundleURL.URLByAppendingPathComponent("\(x).plist")
+    return try? String(contentsOfURL: path)
+  }
+
+}
